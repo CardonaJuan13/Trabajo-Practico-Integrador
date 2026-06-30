1663625b5bfe1911c4608baf3cec41f209a06f85
Cardona Juan David <juanestudios12@gmail.com>
2026-06-30 15:31:38 -0300

    Integracion de JavaScript para carga de productos

---
+++ js/productos.js
@@ -0,0 +1,78 @@
+const endpoint = "../data/api.json";
+const contenedorAccesorios = document.querySelector("#section-accesorios");
+const mensajeAccesorios = document.querySelector("#mensaje-accesorios");
+const contenedorComponentes = document.querySelector("#section-componentes");
+const mensajeComponentes = document.querySelector("#mensaje-componentes");
+const contenedorNotebooks = document.querySelector("#section-notebooks");
+const mensajeNotebooks = document.querySelector("#mensaje-notebooks");
+const contenedorMultimedia = document.querySelector("#section-multimedia");
+const mensajeMultimedia = document.querySelector("#mensaje-multimedia");
+
+function crearCardProducto(producto) {
+ const imagen = producto.pictures && producto.pictures.length > 0
+        ? producto.pictures[0]
+        : "./img/placeholder.jpg";
+ return `
+       <div class="card">
+      <img src="${imagen}" alt="${producto.title}">
+      <h2>${producto.title}</h2>
+      <p>${producto.description}</p>
+      <p class="precio">$${producto.price}</p>
+      <a class="btn detalle" href="./${detalleMap[producto.id]}">Ver detalles</a>
+    </div>
+ `;
+
+}
+function renderizarProductos(productos, contenedor, mensaje) {
+ contenedor.innerHTML = "";
+ mensaje.textContent = "";
+if (productos.length === 0) {
+        mensaje.textContent = "No se encontraron productos.";
+        return;
+ }
+ productos.forEach(producto => {
+        contenedorAccesorios.innerHTML += crearCardProducto(producto);
+ });
+}
+
+const detalleMap = {
+       1: "detalle-kumara.html",
+       2: "detalle-g203.html",
+       3: "detalle-HyperX.html",
+       4: "detalle-AuriAsusRog.html",
+};
+
+async function obtenerProductos() {
+    try {
+        mensajeAccesorios.textContent = "Cargando productos...";
+        mensajeComponentes.textContent = "Cargando componentes...";
+        mensajeNotebooks.textContent = "Cargando notebooks...";
+        mensajeMultimedia.textContent = "Cargando multimedia...";
+        const response = await fetch(endpoint, {
+        headers: { "accept": "api.json" }
+});
+if (!response.ok) {
+        throw new Error("No se pudo obtener la información.");
+}
+    const data = await response.json();
+    console.log(response);
+    console.log(data);
+
+    const accesorios = data.filter(p => p.category === "Accesorios")
+    const componentes = data.filter(p => p.category === "Componentes")
+    const notebooks = data.filter(p=> p.category === "Notebooks")
+    const multimedia = data.filter(p=> p.category === "Multimedia")
+
+    renderizarProductos(accesorios, contenedorAccesorios, mensajeAccesorios);
+    renderizarProductos(componentes,contenedorComponentes,mensajeComponentes);
+    renderizarProductos(notebooks,contenedorNotebooks,mensajeNotebooks);
+    renderizarProductos(multimedia, contenedorMultimedia,mensajeMultimedia);
+} catch (error) {
+       mensajeAccesorios.textContent = "No se pudieron cargar los accesorios.";
+       mensajeComponentes.textContent = "No se pudieron cargar los componentes";
+       mensajeNotebooks.textContent = "No se pudieorn cargar las notebooks";
+       mensajeMultimedia.textContent = "No se pudo cargar el Multimedia";
+    console.error(error);
+ }
+}
+obtenerProductos();
